c6920c74bbf730c03bc207a6dc1369b74190daf8
Sam Jacobson <34726399+samja123@users.noreply.github.com>
2021-01-15 11:07:04 -0500

    Add files via upload

---
+++ cents_to_dollars_and_cents.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<cmath>
+#include<string>
+#include <iostream>
+using namespace std;
+
+const int CENTS_IN_A_DOLLAR = 100;
+const int VALUE_OF_QUARTER = 25;
+const int VALUE_OF_DIME = 10;
+const int VALUE_OF_NICKEL = 5;
+const int VALUE_OF_PENNY = 1;
+int main() {
+    int quarters;
+    int dimes;
+    int nickels;
+    int pennies;
+    int dollars;
+    int cents;
+
+    cout<<"Please enter the number of coins: "<<endl;
+    cout<<"# of quarters: ";
+    cin>>quarters;
+    cout<<"# of dimes: ";
+    cin>>dimes;
+    cout<<"# of nickels: ";
+    cin>>nickels;
+    cout<<"# of pennies: ";
+    cin>>pennies;
+
+    quarters = quarters * VALUE_OF_QUARTER;
+    dimes = dimes * VALUE_OF_DIME;
+    nickels = nickels * VALUE_OF_NICKEL;
+    pennies = pennies * VALUE_OF_PENNY;
+
+    dollars = (quarters + dimes + nickels + pennies) / CENTS_IN_A_DOLLAR;
+    cents = (quarters + dimes + nickels + pennies) % CENTS_IN_A_DOLLAR;
+
+    cout<<"The total is "<<dollars<<" dollars and "<<cents<<" cents";
+
+    return 0;
+}
